fb4c15afa071cb21223aaf8fe557a287a5ac738a
Tomas Hogan <g00467388@atu.ie>
2026-04-20 23:22:27 +0100

    Added create post page

---
+++ Frontend/src/app/create-post/create-post.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonInput, IonButton } from '@ionic/angular/standalone';
+import { HttpClient } from '@angular/common/http';
+import { Post } from '../models/post';
+@Component({
+  selector: 'app-create-post',
+  templateUrl: './create-post.page.html',
+  styleUrls: ['./create-post.page.scss'],
+  standalone: true,
+  imports: [IonInput, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+})
+export class CreatePostPage implements OnInit {
+
+  post :Post = new Post(); 
+  constructor(private httpClient :HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  async submit() {
+    
+    this.httpClient.post("http://localhost:5188/post", this.post).subscribe();
+  }
+
+}
