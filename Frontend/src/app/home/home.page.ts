5b78df2d3502262ed0b490b51855e69e2fe71aac
Tomas Hogan <g00467388@atu.ie>
2026-04-22 13:05:36 +0100

    Added comment input field when viewing post

--- Frontend/src/app/home/home.page.ts
+++ Frontend/src/app/home/home.page.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonInput, IonItem, IonButton, IonButtons, IonModal, IonList, IonLabel, IonText } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonInput, IonItem, IonButton, IonButtons, IonModal, IonList, IonLabel, IonText, IonInfiniteScroll } from '@ionic/angular/standalone';
 import { PostService } from '../services/post-service';
 import { PostResponse } from '../models/postResponse';
+import { Comment } from '../models/comment';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommentRequest } from '../models/commentRequest';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonText, IonLabel, IonList, IonModal, IonButtons, IonButton, IonItem, IonInput, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonSearchbar, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonText, IonLabel, IonList, IonModal, IonButtons, IonButton, IonItem, IonInput, IonCardContent, IonCardSubtitle, 
+    IonCardTitle, IonCardHeader, IonCard, IonSearchbar, IonHeader, IonToolbar, IonTitle, IonContent, IonInfiniteScroll, FormsModule],
 })
 export class HomePage implements OnInit {
-  constructor(private postService : PostService) {}
-  // bound to search bar
+  constructor(private postService : PostService, private httpclient : HttpClient) {}
   searchQuery :string = ""
   initialPosts :PostResponse[] = []
+  comment : CommentRequest = new(CommentRequest)
+
 
   ngOnInit(): void {
     this.postService.GetAllPosts().subscribe(response => 
@@ -28,6 +34,17 @@ openPost(post: PostResponse) {
   this.selectedPost = post;
 }
 
+submitComment() {
+  if (this.comment === null)
+    return; 
+  if (this.selectedPost?.id !== undefined)
+    this.comment.postId = this.selectedPost?.id
+  console.log(this.comment);
+  this.httpclient.post<CommentRequest>("http://localhost:5188/comment", this.comment, {withCredentials: true} ).subscribe();
+  
+  this.comment.content = ""
+  this.comment.postId = 0
+}
 closePost() {
   this.selectedPost = null;
 }
