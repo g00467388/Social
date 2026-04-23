f4055bfb4375487a169f71df0c47840e71b3e83d
Tomas Hogan <g00467388@atu.ie>
2026-04-23 23:58:34 +0100

    Re route user back to home page after logging in

--- Frontend/src/app/authenticate/authenticate.page.ts
+++ Frontend/src/app/authenticate/authenticate.page.ts
@@ -1,51 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonText, IonLabel, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonText, IonLabel, IonButton, ToastController, LoadingController, IonButtons, IonIcon } from '@ionic/angular/standalone';
 import { Auth } from '../models/auth';
 import { HttpClient } from '@angular/common/http';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../services/auth-service';
 
 @Component({
   selector: 'app-authenticate',
   templateUrl: './authenticate.page.html',
   styleUrls: ['./authenticate.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonText, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+  imports: [
+    IonLabel, IonText, IonInput, IonContent, IonHeader, IonTitle, IonToolbar,
+    CommonModule, FormsModule, IonButton, IonButtons, IonIcon, RouterLink, RouterLinkActive
+  ]
 })
-export class AuthenticatePage implements OnInit {
-  auth :Auth = new(Auth);
-  isSignInPage :boolean = true;
+export class AuthenticatePage {
 
+  auth: Auth = new Auth();
+  isSignupPage: boolean = true;
 
-  constructor(private httpclient :HttpClient) { 
-    
-  }
-  
-  ngOnInit() { 
+  constructor(
+    private authService : AuthService
+  ) {}
 
-  }
-
-  SwapAuthPage()
-  {
-    this.isSignInPage = !this.isSignInPage;
+  SwapAuthPage() {
+    this.isSignupPage = !this.isSignupPage;
   }
 
   async submit() {
-    if (this.isSignInPage)
-      this.submitSignup(); 
-    else 
-      this.submitLogin();
+    if (this.isSignupPage)
+      this.authService.submitSignup(this.auth);
+    else
+      this.authService.submitLogin(this.auth);
   }
 
-  private async submitLogin() {
-    this.httpclient.post("http://localhost:5188/auth/login", this.auth, {withCredentials: true}).subscribe(response => {
-      console.log(response);
-    })
-  }
-
-  private async submitSignup() {
-    this.httpclient.post("http://localhost:5188/auth/register", this.auth, {withCredentials: true, observe: 'response'}).subscribe(response => {
-      console.log(response);
-    })
-  }
 }
--- Frontend/src/app/create-post/create-post.page.ts
+++ Frontend/src/app/create-post/create-post.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonInput, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonInput, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
 import { HttpClient } from '@angular/common/http';
 import { Post } from '../models/post';
+import { Endpoints } from '../helpers/endpoints';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.page.html',
   styleUrls: ['./create-post.page.scss'],
   standalone: true,
-  imports: [IonInput, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+  imports: [IonInput, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonButtons, IonIcon, RouterLink, RouterLinkActive]
 })
 export class CreatePostPage {
 
@@ -21,7 +23,7 @@ export class CreatePostPage {
 
   async submit() {
     console.log(this.post);
-    this.httpClient.post("http://localhost:5188/post", this.post, {withCredentials: true}).subscribe();
+    this.httpClient.post(Endpoints.REQUEST_POST, this.post, {withCredentials: true}).subscribe();
   }
 
 }
