b26c63fb09463d4c9ebe027bab81b75d3d198279
Tomas Hogan <g00467388@atu.ie>
2026-04-24 16:42:05 +0100

    Remove accidently included files

--- Backend/Dtos/UserDto.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-public class UserDto 
-{
-   public string Username {get; set;} = string.Empty; 
-   public string Password {get; set;} = string.Empty;
-}
--- Backend/Services/IService.cs
+++
@@ -1,7 +0,0 @@
-public interface IService<T>
-{
-    public Task<bool> DeleteItemByID(Guid id);
-    public Task<T?> InsertAsync(T item);
-    public Task<T?> GetByIdAsync(Guid id);
-    public Task<IEnumerable<T>> GetAllAsync();
-}
